449239c2dc8c61594c2b7f49c5d17df30a313e58
zkfy <lib4net@163.com>
2019-12-30 14:34:42 +0800

    优化 代码

--- init.go
+++ init.go
@@ -2,31 +2,11 @@ package main
 
 import (
 	"github.com/micro-plat/hydra/component"
-	"github.com/micro-plat/smartcar/services"
 )
 
 //init 检查应用程序配置文件，并根据配置初始化服务
-func (r *smartcar) init() {
-	r.Initializing(func(c component.IContainer) error {
-		go services.Forward()
-		//appconf.func#//
-		//#appconf.func//
-
-		//db.init#//
-		//#db.init//
-
-		//cache.init#//
-		//#cache.init//
-
-		//queue.init#//
-		//#queue.init//
-
-		//login.router#//
-		//#login.router//
-
-		//service.router#//
-		//#service.router//
-
+func init() {
+	app.Initializing(func(c component.IContainer) error {
 		return nil
 	})
 }
--- main.go
+++ main.go
@@ -3,23 +3,13 @@ package main
 import "github.com/micro-plat/hydra/hydra"
 import _ "github.com/go-sql-driver/mysql"
 
-type smartcar struct {
-	*hydra.MicroApp
-}
+var app = hydra.NewApp(
+	hydra.WithPlatName("smartcar"),
+	hydra.WithSystemName("smartcar"),
+	hydra.WithServerTypes("api"),
+	hydra.WithDebug())
 
 //CGO_ENABLED=0 GOOS=linux GOARCH=arm go install
 func main() {
-	app := &smartcar{
-		hydra.NewApp(
-			hydra.WithPlatName("smartcar"),
-			hydra.WithSystemName("smartcar"),
-			hydra.WithServerTypes("api"),
-			hydra.WithDebug()),
-	}
-
-	app.init()
-	app.install()
-	app.handling()
-
 	app.Start()
 }
